b4d85e5971a686a31f814f8a7433d0d8680942f9
Feitosa <mathe.feitosa@gmail.com>
2023-04-06 12:40:19 -0300

    Criado modelo profiles

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,46 @@
+from django.contrib.auth.models import User
 from django.db import models
+import uuid
 
-# Create your models here.
+# CONSTANTES
+STATE_CHOICES = (
+    ("AC", "Acre"),
+    ("AL", "Alagoas"),
+    ("AP", "Amapá"),
+    ("AM", "Amazonas"),
+    ("BA", "Bahia"),
+    ("CE", "Ceará"),
+    ("DF", "Distrito Federal"),
+    ("ES", "Espírito Santo"),
+    ("GO", "Goiás"),
+    ("MA", "Maranhão"),
+    ("MT", "Mato Grosso"),
+    ("MS", "Mato Grosso do Sul"),
+    ("MG", "Minas Gerais"),
+    ("PA", "Pará"),
+    ("PB", "Paraíba"),
+    ("PR", "Paraná"),
+    ("PE", "Pernambuco"),
+    ("PI", "Piauí"),
+    ("RJ", "Rio de Janeiro"),
+    ("RN", "Rio Grande do Norte"),
+    ("RS", "Rio Grande do Sul"),
+    ("RO", "Rondônia"),
+    ("RR", "Roraima"),
+    ("SC", "Santa Catarina"),
+    ("SP", "São Paulo"),
+    ("SE", "Sergipe"),
+    ("TO", "Tocantins"),
+)
+
+# MODELOS
+class profile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+    name = models.CharField(max_length=200)
+    cpf = models.CharField(max_length=11)
+    crm_number = models.CharField(max_length=20)
+    crm_state = models.CharField(max_length=2, choices=STATE_CHOICES)
