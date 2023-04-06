a000e8d8603af310d8614bcd68db6d3ba96d4cc3
Feitosa <mathe.feitosa@gmail.com>
2023-04-06 14:32:54 -0300

    Implemented login

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,5 +4,6 @@ from profiles import views
 
 urlpatterns = [
     path("", views.profiles, name="profiles"),
+    path('login/', views.login_page, name='login'),
     path("create/", views.create_profile, name="create_profiles"),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,35 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
 from profiles.forms import ProfileForm
+from .models import Profile
 
 # Create your views here.
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            print('Usuário não existe!')
+        
+        user = authenticate(request, username=username,password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+    return render(request, 'login.html')
+
+def logou_user(request):
+    logout(request)
+    return redirect('profiles')
+
+
 def profiles(request):
-    return HttpResponse("Profiles page!")
+    profiles = Profile.objects.all()
+    return render(request, 'profiles/profiles_list.html', {'profiles':profiles})
 
 
 def create_profile(request):
