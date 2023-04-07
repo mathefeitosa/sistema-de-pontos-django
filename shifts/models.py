67a6c1da4de59888dd2613db32f07f2a8bd096b8
Feitosa <mathe.feitosa@gmail.com>
2023-04-07 16:11:54 -0300

    Formatted models

--- shifts/models.py
+++ shifts/models.py
@@ -10,10 +10,16 @@ from workplaces.models import Workplace
 
 class Shift(models.Model):
     id = models.UUIDField(
-        default=uuid.uuid4, unique=True, editable=False, primary_key=True
+        default=uuid.uuid4,
+        unique=True,
+        editable=False,
+        primary_key=True,
     )
     owner_profile = models.ForeignKey(
-        Profile, verbose_name="Perfil do usuário", on_delete=models.PROTECT, null=True
+        Profile,
+        verbose_name="Perfil do usuário",
+        on_delete=models.PROTECT,
+        null=True,
     )
     workplace = models.ForeignKey(
         Workplace,
@@ -21,24 +27,44 @@ class Shift(models.Model):
         null=False,
         verbose_name="Local de trabalho",
     )
-    begin = models.DateTimeField(auto_now_add=True,
-                                 verbose_name="Início", editable=False
-                                 )
+    begin = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Início",
+        editable=False,
+    )
     end = models.DateTimeField(
-        default=None, auto_now=False, auto_now_add=False, verbose_name="Fim", null=True, blank=True
+        default=None,
+        auto_now=False,
+        auto_now_add=False,
+        verbose_name="Fim",
+        null=True,
+        blank=True,
     )
     begin_position = models.CharField(
-        max_length=200, verbose_name="Posição no início")
+        max_length=200,
+        verbose_name="Posição no início",
+    )
     end_position = models.CharField(
-        max_length=200, verbose_name="Posição no fim", blank=True)
+        max_length=200,
+        verbose_name="Posição no fim",
+        blank=True,
+    )
     begin_photo = models.ImageField(
-        upload_to="static/shift_photos", verbose_name="Foto de entrada", blank=True, null=True
+        upload_to="static/shift_photos",
+        verbose_name="Foto de entrada",
+        blank=True,
+        null=True,
     )
     end_photo = models.ImageField(
-        upload_to="static/shift_photos", verbose_name="Foto de saída", blank=True, null=True
+        upload_to="static/shift_photos",
+        verbose_name="Foto de saída",
+        blank=True,
+        null=True,
     )
     is_open = models.BooleanField(
-        default=True, verbose_name="Ponto está em aberto?")
+        default=True,
+        verbose_name="Ponto está em aberto?",
+    )
 
     def __str__(self):
         if self.owner_profile:
--- workplaces/models.py
+++ workplaces/models.py
@@ -8,19 +8,43 @@ import uuid
 
 class Workplace(models.Model):
     id = models.UUIDField(
-        default=uuid.uuid4, unique=True, editable=False, primary_key=True
+        default=uuid.uuid4,
+        unique=True,
+        editable=False,
+        primary_key=True,
     )
     owner_profile = models.ForeignKey(
-        Profile, verbose_name="Perfil do usuário", on_delete=models.SET_NULL, null=True
+        Profile,
+        verbose_name="Perfil do usuário",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
     )
-    name = models.CharField(verbose_name='Nome do local', max_length=200)
-    team = models.CharField(verbose_name='Equipe', max_length=200)
-    address = models.CharField(verbose_name='Endereço', max_length=200)
-    location = models.CharField(verbose_name='Localização', max_length=200)
-    turn_size_hours = models.IntegerField(verbose_name='Horas diárias')
-    hour_price = models.DecimalField(verbose_name='Preço por hora',
-                                     max_digits=6, decimal_places=2, default=Decimal("0.00")
-                                     )
+    name = models.CharField(
+        verbose_name='Nome do local',
+        max_length=200,
+    )
+    team = models.CharField(
+        verbose_name='Equipe',
+        max_length=200,
+    )
+    address = models.CharField(
+        verbose_name='Endereço',
+        max_length=200,
+    )
+    location = models.CharField(
+        verbose_name='Localização',
+        max_length=200,
+    )
+    turn_size_hours = models.IntegerField(
+        verbose_name='Horas diárias',
+    )
+    hour_price = models.DecimalField(
+        verbose_name='Preço por hora',
+        max_digits=6,
+        decimal_places=2,
+        default=Decimal("0.00"),
+    )
 
     def __str__(self):
         return str(self.name)
